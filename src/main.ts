04c37143d9ce8d8a8c58567bbd0911bc8cc37cfa
xing.wu <wuxing@thunisedu.com>
2021-11-24 14:00:07 +0800

    feat: 添加route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-// import '@/styles/index.scss'
 import '@/styles/index.scss'
+import router from '@/router/index'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
 
